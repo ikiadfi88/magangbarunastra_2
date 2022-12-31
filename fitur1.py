75d8e1ad4e61db173d8320e9be3a4f0b12314692
ikiadfi88 <ikiadfi15@gmail.com>
2022-12-31 18:58:26 +0700

    menambahkan halo lagi

--- fitur1.py
+++ fitur1.py
@@ -1,3 +1,2 @@
-print("\n=====SELAMAT DATANG DI KONVERSI REAMUR KE KELVIN=====\n")
-#Reamur ke Kelvin
+print("\n=====HALO SELAMAT DATANG DI KONVERSI REAMUR KE KELVIN=====\n")
 print("-----KONVERSI DARI REAMUR KE KELVIN-----")
